20e5e4a7ba1b08bba264256b21fef3c92f97af04
Fred de Borst <felison@gmx.net>
2014-01-12 22:24:24 +0100

    Update clear.sh

--- opt/jacksafe/bin/clear.sh
+++ opt/jacksafe/bin/clear.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # Remove contents of jackuser home directory
-sudo rm -rf /home/jackuser/*
+# sudo rm -rf /home/jackuser/*
+
+# this is too dangerous
+# new code needed using rsync to gracefully scan and change only changed and added files
